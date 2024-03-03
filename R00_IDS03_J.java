73ee7d02c819391a43249c5617feb6b26605cd8e
Kzzy <161765757+kzzyc@users.noreply.github.com>
2024-03-03 18:03:55 -0500

    Add files via upload

---
+++ R00_IDS03_J.java
@@ -0,0 +1,12 @@
+// Rule 00. Input Validation and Data Sanitization (IDS) 
+
+if (loginSuccessful) {
+    logger.severe("User login succeeded for: " + sanitizeUser(username));
+} else {
+    logger.severe("User login failed for: " + sanitizeUser(username));
+}
+
+public String sanitizeUser(String username) {
+    return Pattern.matches("[A-Za-z0-9]+", username) ? username : "unauthorized user";
+}
+
